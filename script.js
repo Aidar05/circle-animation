d8b1f8493e46e13f4fbbe115aace465efa1ccc75
VeTeR04 <115419060+VeTeR04@users.noreply.github.com>
2022-10-21 22:43:25 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function enlarge(){
+  document.querySelector('.loader triangle').style.width = '150px'
+  document.querySelector('.loader triangle').style.height = '150px'
+}
+
+function reduce(){
+  document.querySelector('.loader triangle').style.width = '50px'
+  document.querySelector('.loader triangle').style.height = '50px'
+}
+
+function def(){
+  document.querySelector('.loader triangle').style.width = '100px'
+  document.querySelector('.loader triangle').style.height = '100px'
+}
